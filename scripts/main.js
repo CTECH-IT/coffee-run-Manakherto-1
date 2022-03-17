d31ad61ac0c4075a855fdc2c5e66f3fb30513b52
manakherto <9261406@isd535.org>
2022-03-17 11:27:21 -0500

    delete some stuff by accident and add class to the header and created datastore

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
-// Put your JavaScript here
+(function (window) {
+    'use strict';
+    let App = window.App || {};
+    
+
+ DataStore.prototype.add = function(key, val) {
+    this.data = {};
+}
+
+DataStore.prototype.get = function (key) {
+    return this.data[key];
+} 
+DataStore.prototype.getAll =function () {
+    return this.data;
+} 
+DataStore.prototype.remove= function(key) {
+    delete this.dat[key];
+}
+App.DataStore = DataStore;
+window.App = App;
+
+}) (window);
