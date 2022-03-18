cc5f76308c71b89bcb8d04cfe591e01d69b812c7
manakherto <9261406@isd535.org>
2022-03-18 11:17:48 -0500

    add more script and forms info

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,34 @@
 (function (window) {
-    'use strict';
-    let App = window.App || {};
-    
+    'use strict'; 
+    let FORM_SELECTOR = '[data-coffee-order="form"]';
+    let App = window.App;
+    let Truck = App.Truck;
+    let DataStore = App.DataStore;
+    let FormHandler = App.FormHandler;
+    let myTruck = newTruck('12344',new DataStore());
+    let $ = window.jQuery;
+    window.myTruck = myTruck;
 
+    // find the form that is being submitted and a create a formHandler objectS
+    let formHandler = new FormHandler (FORM_SELECTOR);
+    // bind the createOder to a specific truck and pass it to addSubmitHandler
+    formHandler.addSubmitHandler(myTruck.createOder.bind(myTruck)); 
+    console.log(formHandler);
+
+})(window);
+ 
+
+
+function FormHandler(selector){
+    if (!selector) {
+        throw new Error('No selector provided')
+    }
+} 
+// find the "selector" in the DOM using jQuery and assign it to this.formElement
+this.$formElement = $(selector);
+if (this.$formElement.length ==0){
+    throw new Error ('could not find element with selector:' + selector);
+} 
  DataStore.prototype.add = function(key, val) {
     this.data = {};
 }
@@ -18,5 +44,25 @@ DataStore.prototype.remove= function(key) {
 }
 App.DataStore = DataStore;
 window.App = App;
-
 }) (window);
+
+//Add an event handler for the Submit button and pass increateOrder as a parameter (func)
+FormHandler.prototype.addSubmitHandler = function (func) {
+    console.log('Setting the submit handler for the form...');
+    this.$formElement.on('submit', function(event) {
+        event.preventDefault ();
+        //copy all the infor from the form fields into the variable called data
+        let data = {};
+        $(this).serializeArray().forEach(function(item){
+            data[item.name] = item.value;
+            console.log(item.name + 'is ' + item.value);
+        })
+        console.log(data);
+        func(data); // call the function that was passed in on the data from the form
+
+        this.rest(); // rest the form
+        this.element[0].focus(); // focus on the first field
+    });
+}
+
+App.FormHandler = FormHandler;
